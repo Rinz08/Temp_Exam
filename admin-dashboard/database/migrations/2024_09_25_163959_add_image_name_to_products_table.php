2c8387cd4307624fdbb406c39b45eb7c2b9cb589
Rinzzz <rinzlrgamer@gmail.com>
2024-09-26 00:41:50 +0800

    Updated Migration file

--- admin-dashboard/database/migrations/2024_09_25_163959_add_image_name_to_products_table.php
+++ admin-dashboard/database/migrations/2024_09_25_163959_add_image_name_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products_table', function (Blueprint $table) {
-            //
+            $table->string('image_name')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products_table', function (Blueprint $table) {
-            //
+            $table->dropColumn('image_name');
         });
     }
 };
